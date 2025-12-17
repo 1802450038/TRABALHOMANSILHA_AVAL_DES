df25d2013ebea932a2bca73b4c6a154d6247f4d3
1802450038 <gabriel.chaus@gmail.com>
2025-12-17 21:10:22 +0000

    enviando projeto

--- php/server_swoole.php
+++ php/server_swoole.php
@@ -3,22 +3,12 @@ use OpenSwoole\Http\Server;
 use OpenSwoole\Http\Request;
 use OpenSwoole\Http\Response;
 
-// Cria o servidor
+// Cria o servidor na porta 8081
 $server = new Server("127.0.0.1", 8081);
 
-// --- ADICIONE ESTE BLOCO ---
-$server->set([
-    'worker_num' => 32, // Define 4 processos (ajuste conforme seus núcleos de CPU)
-    'reactor_num' => 32 // Threads para gerenciar conexões TCP
-]);
-// ---------------------------
-
 $server->on("Start", function(Server $server) {
-    echo "Swoole server running with 32 workers at http://127.0.0.1:8081\n";
+    echo "Swoole http server is started at http://127.0.0.1:8081\n";
 });
-
-// ... resto do código igual (on Request, etc) ...
-
 
 $server->on("Request", function(Request $request, Response $response) {
     // Configura o header
